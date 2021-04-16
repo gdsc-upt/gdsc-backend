2552cd3cdf4ccfd313baa231d60372b98a80faff
Andreas <andreas.lang@student.upt.ro>
2021-04-16 22:52:46 +0300

    Created Setting Class

---
+++ gdsc-web-backend/Models/Setting.cs
@@ -0,0 +1,11 @@
+namespace gdsc_web_backend.Models
+{
+    public class Setting
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Text { get; set; }
+    }
+}
