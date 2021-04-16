a95b01ac1f8e17e8d7cecd64fd93ed6247c8e7be
Văgă <george.vagauna@student.upt.ro>
2021-04-16 22:17:58 +0300

    Added Faq Model&Controller

---
+++ gdsc-web-backend/Controllers/FaqController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using gdsc_web_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gdsc_web_backend.Controllers
+{
+    
+    [ApiController] 
+    [Route("api/[controller]")]
+    public class FaqController: ControllerBase
+    {
+        
+        [HttpGet]
+        public List<FaqModel> Get()
+        {
+            return new List<FaqModel>
+            {
+
+                new FaqModel
+                {
+                    Id="1",
+                    Question = "Ce faci ?",
+                    Answer = "Uite bine."
+                },
+                
+                new FaqModel
+                {
+                    Id="2",
+                    Question = "Apropo,tu ce faci?",
+                    Answer = "Si mai bine decat tine"
+                }
+            };
+        }
+    }
+}
