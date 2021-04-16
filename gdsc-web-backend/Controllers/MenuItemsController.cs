4194b2352c296f32e30a5b0e5b1a959c5f1918af
Robert Hevesi <roberthevesi09@gmail.com>
2021-04-16 21:56:47 +0300

    added menu items models & controller

---
+++ gdsc-web-backend/Controllers/MenuItemsController.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Xml.Linq;
+using gdsc_web_backend.Models;
+using gdsc_web_backend.Models.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gdsc_web_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class MenuItemsController : ControllerBase
+    {
+        public List<MenuItemModel> MenuItems = new List<MenuItemModel>
+        {
+            new MenuItemModel
+            {
+                Id = "1",
+                Name = "site color",
+                Type = TypeEnum.ExternalLink,
+                Link = "www.google.com"
+            },
+            new MenuItemModel
+            {
+                Id = "2",
+                Name = "language",
+                Type = TypeEnum.InternalLink,
+                Link = "www.linkedin.com"
+            }
+        };
+        
+        [HttpGet]
+        public List<MenuItemModel> Get()
+        {
+            return MenuItems;
+        }
+    
+        [HttpGet("{id}")]
+        public MenuItemModel Get(string id)
+        {
+            return MenuItems.Find(x => x.Id == id);
+        }
+    }
+}
---
+++ gdsc-web-backend/Models/Enums/TypeEnum.cs
@@ -0,0 +1,8 @@
+namespace gdsc_web_backend.Models.Enums
+{
+    public enum TypeEnum
+    {
+        InternalLink,
+        ExternalLink
+    }
+}
