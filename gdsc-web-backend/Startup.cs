7e0f30c4d85d3217fba9a19ca964800bf6e1f4de
GramescuDan <gramescudan@yahoo.com>
2021-04-16 22:23:11 +0300

    database service and connection added

--- gdsc-web-backend/Startup.cs
+++ gdsc-web-backend/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using gdsc_web_backend.Database;
 
 namespace gdsc_web_backend
 {
