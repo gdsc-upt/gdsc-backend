b82fed9e1a9bc674a7723d80bf5b80a66c8e72a0
deancrainic <dean.crainic@gmail.com>
2021-04-16 21:50:43 +0300

    added TeamModel and TeamController

---
+++ gdsc-web-backend/Controllers/TeamController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using gdsc_web_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gdsc_web_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    public class TeamController : ControllerBase
+    {
+        [HttpGet]
+        public List<TeamModel> Get()
+        {
+            return new List<TeamModel>
+            {
+                new TeamModel
+                {
+                    Id = "1",
+                    Name = "Pisicile Salbatice"
+                },
+
+                new TeamModel
+                {
+                    Id = "2",
+                    Name = "FC Barcelona"
+                }
+            };
+        }
+        
+    }
+}
---
+++ gdsc-web-backend/Models/TeamModel.cs
@@ -0,0 +1,8 @@
+﻿namespace gdsc_web_backend.Models
+{
+    public class TeamModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
