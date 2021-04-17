94a5950dca34c7ff5d326c9b5550a25f70653173
Văgă <george.vagauna@student.upt.ro>
2021-04-17 16:37:11 +0300

    added Page Model&Controller

---
+++ gdsc-web-backend/Controllers/PagesController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using gdsc_web_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gdsc_web_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PagesController:ControllerBase
+    {
+
+        public List<PageModel> pageModels = new List<PageModel>
+        {
+            new PageModel()
+            {
+                Id = "1",
+                Title = "gdsc-web-backend",
+                Body = "In progress",
+                isPublished = false,
+                Slug = "Home",
+                ShortDescription = "Shows a description about the site",
+                Image = "Smth 'bout Google",
+
+            },
+            new PageModel()
+            {
+                Id = "2",
+                Title = "gdsc-web-backend",
+                Body = "Still in progress",
+                isPublished = false,
+                Slug = "About us",
+                ShortDescription = "Some data about us and how you could get in touch",
+                Image = "Smth 'bout us",
+            }
+        };
+
+
+        [HttpGet("{slug}")]
+        public PageModel  Get(string slug)
+        {
+            return pageModels.Find(page => page.Slug ==slug);
+
+        }
+    }
+}
---
+++ gdsc-web-backend/Models/PageModel.cs
@@ -0,0 +1,13 @@
+﻿namespace gdsc_web_backend.Models
+{
+    public class PageModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public bool isPublished { get; set; }
+        public string Slug { get; set; }
+        public string ShortDescription{ get; set; }
+        public string Image { get; set; }
+    }
+}
