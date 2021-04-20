72d93633d2fe9e4231dc6784572ef984a7597071
deancrainic <dean.crainic@gmail.com>
2021-04-20 23:42:24 +0300

    Modified TeamsController and added TeamsController testing

---
+++ gdsc-web-backend.tests/TeamsControllerTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using gdsc_web_backend.Controllers;
+using gdsc_web_backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace gdsc_web_backend.tests
+{
+    public class TeamsControllerTests : TestingBase
+    {
+        public TeamsControllerTests(ITestOutputHelper outputHelper) : base(outputHelper) {}
+
+        [Fact]
+        public void Post_ReturnsCreatedObject()
+        {
+            var controller = new TeamsController();
+            var team1 = new TeamModel
+            {
+                Id = "1",
+                Name = "FCSB"
+            };
+            var team2 = new TeamModel
+            {
+                Id = "2",
+                Name = "ASU"
+            };
+
+            var added1 = controller.Post(team1).Result as CreatedResult;
+            var added2 = controller.Post(team2).Result as CreatedResult;
+
+            Assert.NotNull(added1);
+            Assert.Equal(StatusCodes.Status201Created, added1.StatusCode);
+            Assert.Equal(team1, added1.Value as TeamModel);
+            
+            Assert.NotNull(added2);
+            Assert.Equal(StatusCodes.Status201Created, added2.StatusCode);
+            Assert.Equal(team2, added2.Value as TeamModel);
+        }
+
+        [Fact]
+        public void Post_ReturnsError_WhenIdNotUnique()
+        {
+            var controller = new TeamsController();
+            var team1 = new TeamModel
+            {
+                Id = "1",
+                Name = "FCSB"
+            };
+            var team2 = new TeamModel
+            {
+                Id = "2",
+                Name = "ASU"
+            };
+            
+            var added1 = controller.Post(team1).Result as CreatedResult;
+            var added2 = controller.Post(team2).Result as BadRequestObjectResult;
+            Assert.NotNull(added2);
+            var error = added2.Value as ErrorViewModel;
+            
+            Assert.NotNull(added1);
+            Assert.Equal(StatusCodes.Status201Created, added1.StatusCode);
+            Assert.Equal(team1, added1.Value as TeamModel);
+
+            Assert.NotNull(error);
+            Assert.Equal(StatusCodes.Status400BadRequest, added2.StatusCode);
+            Assert.Equal("An object with the same ID already exists", error.Message);
+        }
+        
+        [Fact]
+        public void Get_ReturnsAllExamples()
+        {
+            var controller = new TeamsController();
+            var teams = new List<TeamModel>
+            {
+                new()
+                {
+                    Id = "1",
+                    Name = "FCSB"
+                },
+                new()
+                {
+                    Id = "2",
+                    Name = "ASU"
+                }
+            };
+
+            controller.Post(teams[0]);
+            controller.Post(teams[1]);
+            
+            var result = controller.Get().Result as OkObjectResult;
+            
+            Assert.NotNull(result);
+            var items = Assert.IsAssignableFrom<List<TeamModel>>(result.Value);
+            WriteLine(items);
+            Assert.Equal(teams, items);
+        }
+    }
+}
--- gdsc-web-backend/Controllers/TeamsController.cs
+++ gdsc-web-backend/Controllers/TeamsController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using gdsc_web_backend.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gdsc_web_backend.Controllers
@@ -8,24 +9,48 @@ namespace gdsc_web_backend.Controllers
     [Route("api/[controller]")]  
     public class TeamsController : ControllerBase
     {
-        [HttpGet]
-        public List<TeamModel> Get()
+        private readonly List<TeamModel> _mockTeams = new List<TeamModel>
         {
-            return new List<TeamModel>
+            new()
             {
-                new TeamModel
-                {
-                    Id = "1",
-                    Name = "FCB"
-                },
-                new TeamModel
-                {
-                    Id = "2",
-                    Name = "LFC"
-                }
-            };
+                Id = "1",
+                Name = "FCSB"
+            },
+            new()
+            {
+                Id = "2",
+                Name = "asfasf"
+            }
+        };
+        
+        [HttpGet]
+        public ActionResult<TeamModel> Get()
+        {
+            return Ok(_mockTeams);
         }
 
+        [HttpPost]
+        [ProducesResponseType(typeof(ErrorViewModel), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ExampleModel), StatusCodes.Status201Created)]
+        public ActionResult<TeamModel> Post([FromBody] TeamModel entity)
+        {
+            if (entity is null)
+            {
+                return BadRequest(new ErrorViewModel {Message = "Request has no body"});
+            }
+
+            var existing = _mockTeams.Find(e => e.Id == entity.Id);
+            if (existing != null)
+            {
+                return BadRequest(new ErrorViewModel {Message = "An object with the same ID already exists"});
+            }
+
+            _mockTeams.Add(entity);
+            entity = _mockTeams.Find(team => team == entity);
+
+            return Created("api/Teams/" + entity!.Id, entity);
+        }
+        
         [HttpGet("{teamId}/members")]
         public List<MemberModel> GetMembers(string teamId)
         {
@@ -44,7 +69,7 @@ namespace gdsc_web_backend.Controllers
                     Name = "Dorel",
                     Email = "dorel@gigel.com",
                     TeamId = teamId
-                }
+                },
             };
         }
     }
