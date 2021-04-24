8ad97f6deeed137c63167aeb6ab0131895fa1885
GramescuDan <gramescudan@yahoo.com>
2021-04-24 21:09:01 +0300

    Added AuthController

---
+++ gdsc-web-backend/Controllers/AuthenticationController.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNetCore.Http;  
+using Microsoft.AspNetCore.Identity;  
+using Microsoft.AspNetCore.Mvc;  
+using Microsoft.Extensions.Configuration;  
+using Microsoft.IdentityModel.Tokens;  
+using System;  
+using System.Collections.Generic;  
+using System.IdentityModel.Tokens.Jwt;  
+using System.Security.Claims;  
+using System.Text;  
+using System.Threading.Tasks;  
+using gdsc_web_backend.Authentication;
+
+namespace gdsc_web_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<AppUser> userModel;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(UserManager<AppUser> userModel, IConfiguration configuration)
+        {
+            this.userModel = userModel;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)  
+        {  
+            var user = await userModel.FindByNameAsync(model.Username);  
+            if (user != null && await userModel.CheckPasswordAsync(user, model.Password))  
+            {  
+                var userRoles = await userModel.GetRolesAsync(user);  
+  
+                var authClaims = new List<Claim>  
+                {  
+                    new Claim(ClaimTypes.Name, user.UserName),  
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),  
+                };  
+  
+                foreach (var userRole in userRoles)  
+                {  
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));  
+                }  
+  
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));  
+  
+                var token = new JwtSecurityToken(  
+                    issuer: _configuration["JWT:ValidIssuer"],  
+                    audience: _configuration["JWT:ValidAudience"],  
+                    expires: DateTime.Now.AddHours(3),  
+                    claims: authClaims,  
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)  
+                );  
+  
+                return Ok(new  
+                {  
+                    token = new JwtSecurityTokenHandler().WriteToken(token),  
+                    expiration = token.ValidTo  
+                });  
+            }  
+            return Unauthorized();  
+        } 
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model)  
+        {  
+            var userExists = await userModel.FindByNameAsync(model.Username);  
+            if (userExists != null)  
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });  
+  
+            AppUser user = new AppUser()  
+            {  
+                Email = model.Email,  
+                SecurityStamp = Guid.NewGuid().ToString(),  
+                UserName = model.Username  
+            };  
+            var result = await userModel.CreateAsync(user, model.Password);  
+            if (!result.Succeeded)  
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });  
+  
+            return Ok(new Response { Status = "Success", Message = "User created successfully!" });  
+        }  
+}
+
+}
