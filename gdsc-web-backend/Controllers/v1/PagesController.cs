fad7fdad34ef7ed55d10a37a7ffebb4500fdc6f1
Văgă <george.vagauna@student.upt.ro>
2021-05-14 20:20:21 +0300

    Updated PagesController

--- gdsc-web-backend/Controllers/v1/PagesController.cs
+++ gdsc-web-backend/Controllers/v1/PagesController.cs
@@ -1,6 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gdsc_web_backend.Database;
 using gdsc_web_backend.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PageModel = gdsc_web_backend.Models.PageModel;
 
 namespace gdsc_web_backend.Controllers.v1
 {
@@ -9,34 +15,61 @@ namespace gdsc_web_backend.Controllers.v1
     [Route("api/v1/pages")]
     public class PagesController : ControllerBase
     {
-        public List<PageModel> pageModels = new()
-        {
-            new PageModel
-            {
-                Id = "1",
-                Title = "Home",
-                Body = "In progress",
-                isPublished = false,
-                Slug = "home-page",
-                ShortDescription = "Shows a description about the site",
-                Image = "Smth 'bout Google"
-            },
-            new PageModel
-            {
-                Id = "2",
-                Title = "Contact",
-                Body = "Still in progress",
-                isPublished = false,
-                Slug = "contact",
-                ShortDescription = "Some data about us and how you could get in touch",
-                Image = "Smth 'bout us"
-            }
-        };
+        private readonly IRepository<PageModel> _repository;
 
-        [HttpGet("{slug}")]
-        public PageModel Get(string slug)
+        public PagesController(IRepository<PageModel> repository)
         {
-            return pageModels.Find(page => page.Slug == slug);
+            _repository = repository;
+        }
+        
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<PageModel>>> Get()
+        {
+            return Ok((await _repository.GetAsync()).ToList());
+        }
+
+        // [HttpGet("{slug}")]
+        // [ProducesResponseType(StatusCodes.Status200OK)]
+        // [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        // [ProducesResponseType(StatusCodes.Status404NotFound)]
+        // public async Task<ActionResult<PageModel>> Get([FromRoute] string slug)
+        // {
+        //     var entity = await _repository.GetAsync(slug);
+        //
+        //     return entity is null ? NotFound() : Ok(entity);
+        // }
+        
+        [HttpPost]
+        [ProducesResponseType(typeof(ErrorViewModel), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(PageModel), StatusCodes.Status201Created)]
+        public async Task<ActionResult<PageModel>> Post(PageModel entity)
+        {
+            entity = await _repository.AddAsync(entity);
+
+            return CreatedAtAction(nameof(Post), new {entity.Id}, entity);
+        }
+        
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(PageModel), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<PageModel>> Delete([FromRoute] string id)
+        {
+            var entity = await _repository.DeleteAsync(id);
+
+            return entity is null ? NotFound() : Ok(entity);
+        }
+
+        [HttpPatch]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<MenuItemModel>> Update(PageModel entity)
+        {
+            entity = await _repository.UpdateAsync(entity);
+
+            return CreatedAtAction(nameof(Update), new {entity.Id}, entity);
         }
     }
 }
