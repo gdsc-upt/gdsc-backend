9b87a0c09fe41c8e7c66aba49f1102b227b73ee6
Mateas Mario <39602100+mateasmario@users.noreply.github.com>
2021-05-19 23:00:04 +0300

    Updated TeamsController.cs (#33)

--- GdscBackend/Controllers/v1/TeamsController.cs
+++ GdscBackend/Controllers/v1/TeamsController.cs
@@ -1,52 +1,66 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using gdsc_web_backend.Database;
 using gdsc_web_backend.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gdsc_web_backend.Controllers.v1
 {
     [ApiController]
     [ApiVersion("1")]
+
     [Route("api/v1/teams")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
     public class TeamsController : ControllerBase
     {
-        [HttpGet]
-        public List<TeamModel> Get()
-        {
-            return new()
-            {
-                new TeamModel
-                {
-                    Id = "1",
-                    Name = "FCB"
-                },
-                new TeamModel
-                {
-                    Id = "2",
-                    Name = "LFC"
-                }
-            };
-        }
+        private readonly IRepository<TeamModel> _repository;
 
-        [HttpGet("{teamId}/members")]
-        public List<MemberModel> GetMembers(string teamId)
+        public TeamsController(IRepository<TeamModel> repository)
         {
-            return new()
-            {
-                new MemberModel
-                {
-                    Id = "1",
-                    Name = "Gigel",
-                    Email = "yahoo@gigel.com",
-                    TeamId = teamId
-                },
-                new MemberModel
-                {
-                    Id = "2",
-                    Name = "Dorel",
-                    Email = "dorel@gigel.com",
-                    TeamId = teamId
-                }
-            };
+            _repository = repository;
+        }
+        
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<TeamModel>>> Get()
+        {
+            return Ok((await _repository.GetAsync()).ToList());
+        }
+        
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<TeamModel>> Get([FromRoute] string id)
+        {
+            var entity = await _repository.GetAsync(id);
+
+            return entity is null ? NotFound() : Ok(entity);
+        }
+        
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<TeamModel>> Post(TeamModel entity)
+        {
+            entity = await _repository.AddAsync(entity);
+
+            return CreatedAtAction(nameof(Post), new {entity.Id}, entity);
+        }
+        
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<TeamModel>> Delete([FromRoute] string id)
+        {
+            var entity = await _repository.DeleteAsync(id);
+
+            return entity is null ? NotFound() : Ok(entity);
         }
     }
 }
