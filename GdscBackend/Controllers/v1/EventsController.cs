ced5c71d028b83301b5fa5d4c863d4077a714f61
VagaunaGeorge <75121062+VagaunaGeorge@users.noreply.github.com>
2021-05-19 22:55:41 +0300

    Merge pull request #35 from dsc-upt/updatePagesController
    Update pages controller

--- GdscBackend/Controllers/v1/EventsController.cs
+++ GdscBackend/Controllers/v1/EventsController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using gdsc_web_backend.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gdsc_web_backend.Controllers.v1
@@ -9,25 +10,9 @@ namespace gdsc_web_backend.Controllers.v1
     [Route("api/v1/events")]
     public class EventsController : ControllerBase
     {
-        public List<EventModel> EventModels = new()
-        {
-            new EventModel
-            {
-                Id = "1",
-                Title = "example 1",
-                Description = "description 1",
-                Image = "image link 1"
-            },
-            new EventModel
-            {
-                Id = "1",
-                Title = "example 2",
-                Description = "description 2",
-                Image = "image link 2"
-            }
-        };
+        public List<EventModel> EventModels = new();
 
-        // HTTP Get method without any ID, returning the whole list of events
+            // HTTP Get method without any ID, returning the whole list of events
         [HttpGet]
         public List<EventModel> Get()
         {
@@ -39,6 +24,30 @@ namespace gdsc_web_backend.Controllers.v1
         public EventModel Get(string id)
         {
             return EventModels.Find(eventElement => eventElement.Id == id);
+        }
+
+        // HTTP Post method
+        [HttpPost]
+        [ProducesResponseType(typeof(ErrorViewModel), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(EventModel), StatusCodes.Status201Created)]
+        public ActionResult<ContactModel> Post([FromBody] EventModel entity)
+        {
+            if (entity is null)
+            {
+                return BadRequest(new ErrorViewModel {Message = "Request has no body"});
+            }
+
+            // See if the entity given does already exist
+            var doesExist = EventModels.Find(element => element.Id == entity.Id);
+            if (doesExist != null)
+            {
+                return BadRequest(new ErrorViewModel {Message = $"{entity} already exists"});
+            }
+
+            EventModels.Add(entity);
+
+            entity = EventModels.Find(e => e == entity);
+            return Created("api/Event/" + entity!.Id, entity);
         }
     }
 }
--- GdscBackend/Controllers/v1/FaqsController.cs
+++ GdscBackend/Controllers/v1/FaqsController.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using gdsc_web_backend.Database;
 using gdsc_web_backend.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,38 +12,54 @@ namespace gdsc_web_backend.Controllers.v1
     [ApiController]
     [ApiVersion("1")]
     [Route("api/v1/faqs")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
     public class FaqsController : ControllerBase
     {
-        private readonly List<FaqModel> _mockFaq = new();
+        private readonly IRepository<FaqModel> _repository;
 
-        [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<FaqModel>), StatusCodes.Status200OK)]
-        public ActionResult<IEnumerable<FaqModel>> Get()
+        public FaqsController(IRepository<FaqModel> repository)
         {
-            return Ok(_mockFaq);
+            _repository = repository;
         }
 
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<FaqModel>>> Get()
+        {
+            return Ok((await _repository.GetAsync()).ToList());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<FaqModel>> Get([FromRoute] string id)
+        {
+            var entity = await _repository.GetAsync(id);
+
+            return entity is null ? NotFound() : Ok(entity);
+        }
 
         [HttpPost]
         [ProducesResponseType(typeof(ErrorViewModel), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(FaqModel), StatusCodes.Status201Created)]
-        public ActionResult<FaqModel> Post([FromBody] FaqModel entity)
+        public async Task<ActionResult<FaqModel>> Post(FaqModel entity)
         {
-            if (entity is null)
-            {
-                return BadRequest(new ErrorViewModel {Message = "Request has no body"});
-            }
+            entity = await _repository.AddAsync(entity);
 
-            //create a variable where we return the value of the find function applied on the _mockFaq
-            var doesExist = _mockFaq.Find(model => model.Id == entity.Id);
-            if (doesExist is not null)
-            {
-                return BadRequest(new ErrorViewModel {Message = $"{entity} already exists"});
-            }
+            return CreatedAtAction(nameof(Post), new {entity.Id}, entity);
+        }
 
-            _mockFaq.Add(entity);
-            entity = _mockFaq.Find(m => m == entity);
-            return Ok(entity);
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(ExampleModel), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<FaqModel>> Delete([FromRoute] string id)
+        {
+            var entity = await _repository.DeleteAsync(id);
+
+            return entity is null ? NotFound() : Ok(entity);
         }
     }
 }
