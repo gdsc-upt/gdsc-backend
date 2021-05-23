ca641600833806003a16273e794f676d9c384b33
llNicoleta <idlnicoleta@gmail.com>
2021-05-23 23:27:27 +0300

    Added technology model

---
+++ GdscBackend/Models/TechnologyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace gdsc_web_backend.Models
+{
+    public class TechnologyModel : Model
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
