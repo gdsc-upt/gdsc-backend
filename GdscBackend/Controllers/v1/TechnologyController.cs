fb1a5ab76715b6d57389cb4032647d686ece86a9
Andreea <andreea.bolea@student.upt.ro>
2021-05-24 11:36:24 +0200

    fixed empty lines

--- GdscBackend/Controllers/v1/TechnologyController.cs
+++ GdscBackend/Controllers/v1/TechnologyController.cs
@@ -22,7 +22,6 @@ namespace gdsc_web_backend.Controllers.v1
             _repository = repository;
         }
         
-        //get all
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<TechnologyModel>>> GetAll()
@@ -30,7 +29,6 @@ namespace gdsc_web_backend.Controllers.v1
             return Ok((await _repository.GetAsync()).ToList());
         }
         
-        //post
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -40,20 +38,14 @@ namespace gdsc_web_backend.Controllers.v1
             return CreatedAtAction(nameof(Post), new {entity.Id}, entity);
         }
         
-        //delete
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-
         public async Task<ActionResult<TechnologyModel>> Delete([FromRoute] string id)
         {
             var entity = await _repository.DeleteAsync(id);
             return entity is null ? NotFound() : Ok(entity);
         }
-        
-        
-
-
     }
 }
