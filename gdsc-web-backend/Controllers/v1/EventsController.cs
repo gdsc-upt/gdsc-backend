f1f70efb43d0b0a6a34cab85e710a1abc4d00790
Robert Hevesi <roberthevesi09@gmail.com>
2021-05-24 20:22:27 +0300

    Updated events controller

--- gdsc-web-backend/Controllers/v1/EventsController.cs
+++ gdsc-web-backend/Controllers/v1/EventsController.cs
@@ -1,5 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using gdsc_web_backend.Database;
 using gdsc_web_backend.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gdsc_web_backend.Controllers.v1
@@ -7,38 +12,65 @@ namespace gdsc_web_backend.Controllers.v1
     [ApiController]
     [ApiVersion("1")]
     [Route("api/v1/events")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
     public class EventsController : ControllerBase
     {
-        public List<EventModel> EventModels = new()
-        {
-            new EventModel
-            {
-                Id = "1",
-                Title = "example 1",
-                Description = "description 1",
-                Image = "image link 1"
-            },
-            new EventModel
-            {
-                Id = "1",
-                Title = "example 2",
-                Description = "description 2",
-                Image = "image link 2"
-            }
-        };
+        private readonly IRepository<EventModel> _repository;
 
-        // HTTP Get method without any ID, returning the whole list of events
-        [HttpGet]
-        public List<EventModel> Get()
+        public EventsController(IRepository<EventModel> repository)
         {
-            return EventModels;
+            _repository = repository;
         }
 
-        // HTTP Get method with a specific ID, which will return the event having that ID
-        [HttpGet("{id}")]
-        public EventModel Get(string id)
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public async Task<ActionResult<IEnumerable<EventModel>>> Get()
         {
-            return EventModels.Find(eventElement => eventElement.Id == id);
+            return Ok((await _repository.GetAsync()).ToList());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult<EventModel>> Get([FromRoute] string id)
+        {
+            var entity = await _repository.GetAsync(id);
+            return entity is null ? NotFound() : Ok(entity);
+        }
+        
+        [HttpPost]
+        [ProducesResponseType(typeof(EventModel), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        
+        public async Task<ActionResult<EventModel>> Post(EventModel entity)
+        {
+            entity = await _repository.AddAsync(entity);
+            return CreatedAtAction(nameof(Post), new {entity.Id}, entity);
+        }
+        
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(EventModel), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult<EventModel>> Delete([FromRoute] string id)
+        {
+            var entity = await _repository.DeleteAsync(id);
+            return entity is null ? NotFound() : Ok(entity);
+        }
+        
+        [HttpPatch]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<EventModel>> Update(EventModel entity)
+        {
+            entity = await _repository.UpdateAsync(entity);
+            return CreatedAtAction(nameof(Update), new {entity.Id}, entity);
         }
     }
 }
