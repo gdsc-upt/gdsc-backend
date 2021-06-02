eff9245069229b878918d8f8f727f329a4cedfad
GramescuDan <gramescudan@yahoo.com>
2021-06-02 17:32:32 +0300

    MenuItemsControllerTests-Setup

--- GdscBackend.Tests/MenuItemsControllerTests.cs
+++ GdscBackend.Tests/MenuItemsControllerTests.cs
@@ -1,7 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FactoryBot;
+using GdscBackend.Models;
+using GdscBackend.Models.Enums;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Xunit;
+using Xunit.Abstractions;
+
 namespace GdscBackend.Tests
 {
-    public class MenuItemsControllerTests
+    public class MenuItemsControllerTests : TestingBase
     {
+        private readonly IEnumerable<MenuItemModel> _testData = _getTestData();
+
+        MenuItemsControllerTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+        }
+
+        [Fact]
+        public async void Post_ReturnsCreatedObject()
+        {
+            
+        }
+
+        [Fact]
+        public async void GetById_ReturnsOkObjectResult()
+        {
+            
+        }
+
+        [Fact]
+        public async void Get_ReturnsAllExamples()
+        {
+            
+        }
+
+        [Fact]
+        public async void Delete_Returns404NotFoundResult()
+        {
+            
+        }
+
+        [Fact]
+        public async void Update_ReturnsCreatedAtAction()
+        {
+            
+        }
         
+        private static IEnumerable<MenuItemModel> _getTestData()
+        {
+            Bot.Define(x => new MenuItemModel
+            {
+                Id = x.Strings.Guid(),
+                Name = x.Names.FirstName(),
+                Type = x.Enums.Any<MenuItemTypeEnum>(),
+                Link = Faker.Lorem.Words(3).ToString()
+            });
+
+            return Bot.BuildSequence<MenuItemModel>().Take(10).ToList();
+        }
     }
 }
