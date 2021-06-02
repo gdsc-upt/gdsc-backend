92936112ca37322b9c86ad4a91fb76359d629a63
GramescuDan <gramescudan@yahoo.com>
2021-06-03 01:34:02 +0300

    MenuItemsTests-GetTested

--- GdscBackend.Tests/MenuItemsControllerTests.cs
+++ GdscBackend.Tests/MenuItemsControllerTests.cs
@@ -6,6 +6,8 @@ using GdscBackend.Controllers.v1;
 using GdscBackend.Database;
 using GdscBackend.Models;
 using GdscBackend.Models.Enums;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Xunit;
 using Xunit.Abstractions;
@@ -16,55 +18,53 @@ namespace GdscBackend.Tests
     {
         private readonly IEnumerable<MenuItemModel> _testData = _getTestData();
 
-        MenuItemsControllerTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        public MenuItemsControllerTests(ITestOutputHelper outputHelper) : base(outputHelper)
         {
         }
 
         [Fact]
         public async void Post_ReturnsCreatedObject()
         {
-            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>().Object);
+            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>(_testData).Object);
             var controller = new MenuItemsController(repository);
-            
-            
         }
 
         [Fact]
         public async void GetById_ReturnsOkObjectResult()
         {
-            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>().Object);
+            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>(_testData).Object);
             var controller = new MenuItemsController(repository);
-            
-            
         }
 
         [Fact]
         public async void Get_ReturnsAllExamples()
         {
-            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>().Object);
-            var controller = new MenuItemsController(repository); 
-            
-            
+            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>(_testData).Object);
+            var controller = new MenuItemsController(repository);
+
+            var actionResult = await controller.Get();
+            var returnedResult = actionResult.Result as OkObjectResult;
+            var items = Assert.IsAssignableFrom<IEnumerable<MenuItemModel>>(returnedResult.Value);
+
+            Assert.NotNull(returnedResult);
+            Assert.Equal(StatusCodes.Status200OK, returnedResult.StatusCode);
+            Assert.Equal(_testData.ToList(), items.ToList());
         }
 
         [Fact]
         public async void Delete_Returns404NotFoundResult()
         {
-            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>().Object);
+            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>(_testData).Object);
             var controller = new MenuItemsController(repository);
-            
-            
         }
 
         [Fact]
         public async void Update_ReturnsCreatedAtAction()
         {
-            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>().Object);
+            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>(_testData).Object);
             var controller = new MenuItemsController(repository);
-            
-            
         }
-        
+
         private static IEnumerable<MenuItemModel> _getTestData()
         {
             Bot.Define(x => new MenuItemModel
