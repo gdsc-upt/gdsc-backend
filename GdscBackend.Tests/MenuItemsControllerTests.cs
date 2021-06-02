3dcd2b396de452471d6bddef9333a4bcc0ceb533
GramescuDan <gramescudan@yahoo.com>
2021-06-02 17:55:05 +0300

    AddMenuItemsTests- Added vars to methods

--- GdscBackend.Tests/MenuItemsControllerTests.cs
+++ GdscBackend.Tests/MenuItemsControllerTests.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using FactoryBot;
+using GdscBackend.Controllers.v1;
+using GdscBackend.Database;
 using GdscBackend.Models;
 using GdscBackend.Models.Enums;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
@@ -21,30 +23,45 @@ namespace GdscBackend.Tests
         [Fact]
         public async void Post_ReturnsCreatedObject()
         {
+            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>().Object);
+            var controller = new MenuItemsController(repository);
+            
             
         }
 
         [Fact]
         public async void GetById_ReturnsOkObjectResult()
         {
+            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>().Object);
+            var controller = new MenuItemsController(repository);
+            
             
         }
 
         [Fact]
         public async void Get_ReturnsAllExamples()
         {
+            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>().Object);
+            var controller = new MenuItemsController(repository); 
+            
             
         }
 
         [Fact]
         public async void Delete_Returns404NotFoundResult()
         {
+            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>().Object);
+            var controller = new MenuItemsController(repository);
+            
             
         }
 
         [Fact]
         public async void Update_ReturnsCreatedAtAction()
         {
+            var repository = new Repository<MenuItemModel>(new TestDbContext<MenuItemModel>().Object);
+            var controller = new MenuItemsController(repository);
+            
             
         }
         
