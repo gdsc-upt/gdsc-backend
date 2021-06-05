f3a6a9566ff943b215d5c722c924bc6f4b62b305
Mihai <mihai1309f@gmail.com>
2021-06-06 00:04:12 +0300

    Added FileModel + Controller

---
+++ GdscBackend/Controllers/v1/FilesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using GdscBackend.Database;
+using GdscBackend.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GdscBackend.Controllers.v1
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/v1/files")]
+    public class FilesController : ControllerBase
+    {
+        private readonly IRepository<FileModel> _repository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FilesController(IWebHostEnvironment webHostEnvironment, IRepository<FileModel> repository)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _repository = repository;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<ActionResult> Upload([FromForm] List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            List<FileModel> fileModels = new List<FileModel>();
+            
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    String fileName, fileExtension;
+                    
+                    fileName = formFile.FileName.Split(".")[0];
+                    fileExtension = formFile.FileName.Split(".")[1];
+
+                    var relativePath = Path.Combine("media",
+                        fileName + System.Guid.NewGuid().ToString().Split("-")[0] + "." + fileExtension);
+                    
+                    var filePath = Path.Combine(_webHostEnvironment.ContentRootPath, "..", relativePath);
+                    
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+
+                    var fileModel = new FileModel
+                    {
+                        Name = fileName,
+                        Extension = fileExtension,
+                        Path = relativePath,
+                        Size = formFile.Length
+                    };
+
+                    fileModel = await _repository.AddAsync(fileModel);
+                    
+                    fileModels.Add(fileModel);
+                }
+            }
+
+            return Created("api/v1/files", fileModels);
+        }
+    }
+}
---
+++ GdscBackend/Migrations/20210605202627_AddedFileModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace gdsc_web_backend.Migrations
+{
+    public partial class AddedFileModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Examples",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Path = table.Column<string>(type: "text", nullable: true),
+                    Extension = table.Column<string>(type: "text", nullable: true),
+                    Size = table.Column<long>(type: "bigint", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Updated = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Files");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Examples",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
