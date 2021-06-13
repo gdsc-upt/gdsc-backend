a4cb14eff61223428696dcbd3e1519d7691fb6b0
GramescuDan <gramescudan@yahoo.com>
2021-06-13 21:38:59 +0300

    feat:Event+faq controllers mapped

--- GdscBackend/Controllers/v1/FaqsController.cs
+++ GdscBackend/Controllers/v1/FaqsController.cs
@@ -2,8 +2,10 @@
 using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
+using AutoMapper;
 using GdscBackend.Database;
 using GdscBackend.Models;
+using GdscBackend.RequestModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -18,19 +20,21 @@ namespace GdscBackend.Controllers.v1
     [Produces(MediaTypeNames.Application.Json)]
     public class FaqsController : ControllerBase
     {
+        private readonly IMapper _mapper;
         private readonly IRepository<FaqModel> _repository;
 
-        public FaqsController(IRepository<FaqModel> repository)
+        public FaqsController(IRepository<FaqModel> repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         [AllowAnonymous]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IEnumerable<FaqModel>>> Get()
+        public async Task<ActionResult<IEnumerable<FaqRequest>>> Get()
         {
-            return Ok((await _repository.GetAsync()).ToList());
+            return Ok(Map((await _repository.GetAsync()).ToList()));
         }
 
         [HttpGet("{id}")]
@@ -38,9 +42,9 @@ namespace GdscBackend.Controllers.v1
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<FaqModel>> Get([FromRoute] string id)
+        public async Task<ActionResult<FaqRequest>> Get([FromRoute] string id)
         {
-            var entity = await _repository.GetAsync(id);
+            var entity = Map(await _repository.GetAsync(id));
 
             return entity is null ? NotFound() : Ok(entity);
         }
@@ -49,11 +53,11 @@ namespace GdscBackend.Controllers.v1
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<ActionResult<FaqModel>> Post(FaqModel entity)
+        public async Task<ActionResult<FaqRequest>> Post(FaqRequest entity)
         {
-            entity = await _repository.AddAsync(entity);
+            entity = Map(await _repository.AddAsync(Map(entity)));
 
-            return CreatedAtAction(nameof(Post), new { entity.Id }, entity);
+            return CreatedAtAction(nameof(Post), new {Map(entity).Id}, entity);
         }
 
         [HttpDelete("{id}")]
@@ -61,11 +65,31 @@ namespace GdscBackend.Controllers.v1
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<ActionResult<FaqModel>> Delete([FromRoute] string id)
+        public async Task<ActionResult<FaqRequest>> Delete([FromRoute] string id)
         {
-            var entity = await _repository.DeleteAsync(id);
+            var entity = Map(await _repository.DeleteAsync(id));
 
             return entity is null ? NotFound() : Ok(entity);
+        }
+
+        private FaqModel Map(FaqRequest entity)
+        {
+            return _mapper.Map<FaqModel>(entity);
+        }
+
+        private FaqRequest Map(FaqModel entity)
+        {
+            return _mapper.Map<FaqRequest>(entity);
+        }
+
+        private IEnumerable<FaqRequest> Map(IEnumerable<FaqModel> entity)
+        {
+            return _mapper.Map<IEnumerable<FaqRequest>>(entity);
+        }
+
+        private IEnumerable<FaqModel> Map(IEnumerable<FaqRequest> entity)
+        {
+            return _mapper.Map<IEnumerable<FaqModel>>(entity);
         }
     }
 }
