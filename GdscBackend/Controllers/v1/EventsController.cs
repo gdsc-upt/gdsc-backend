1f708fd49c416a3137c11c224db20f3c66e4b500
GramescuDan <gramescudan@yahoo.com>
2021-06-13 21:38:59 +0300

    feat:Event+faq controllers mapped

--- GdscBackend/Controllers/v1/EventsController.cs
+++ GdscBackend/Controllers/v1/EventsController.cs
@@ -2,8 +2,10 @@
 using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
+using AutoMapper;
 using GdscBackend.Database;
 using GdscBackend.Models;
+using GdscBackend.RequestModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -18,19 +20,21 @@ namespace GdscBackend.Controllers.v1
     [Produces(MediaTypeNames.Application.Json)]
     public class EventsController : ControllerBase
     {
+        private readonly IMapper _mapper;
         private readonly IRepository<EventModel> _repository;
 
-        public EventsController(IRepository<EventModel> repository)
+        public EventsController(IRepository<EventModel> repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         [AllowAnonymous]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IEnumerable<EventModel>>> Get()
+        public async Task<ActionResult<IEnumerable<EventRequest>>> Get()
         {
-            return Ok((await _repository.GetAsync()).ToList());
+            return Ok(Map((await _repository.GetAsync()).ToList()));
         }
 
         [HttpGet("{id}")]
@@ -38,28 +42,28 @@ namespace GdscBackend.Controllers.v1
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<EventModel>> Get([FromRoute] string id)
+        public async Task<ActionResult<EventRequest>> Get([FromRoute] string id)
         {
-            var entity = await _repository.GetAsync(id);
+            var entity = Map(await _repository.GetAsync(id));
             return entity is null ? NotFound() : Ok(entity);
         }
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<EventModel>> Post(EventModel entity)
+        public async Task<ActionResult<EventRequest>> Post(EventRequest entity)
         {
-            entity = await _repository.AddAsync(entity);
-            return CreatedAtAction(nameof(Post), new { entity.Id }, entity);
+            entity = Map(await _repository.AddAsync(Map(entity)));
+            return CreatedAtAction(nameof(Post), new {Map(entity).Id}, entity);
         }
 
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<EventModel>> Delete([FromRoute] string id)
+        public async Task<ActionResult<EventRequest>> Delete([FromRoute] string id)
         {
-            var entity = await _repository.DeleteAsync(id);
+            var entity = Map(await _repository.DeleteAsync(id));
             return entity is null ? NotFound() : Ok(entity);
         }
 
@@ -67,10 +71,30 @@ namespace GdscBackend.Controllers.v1
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<EventModel>> Update(EventModel entity)
+        public async Task<ActionResult<EventRequest>> Update(EventRequest entity)
         {
-            entity = await _repository.UpdateAsync(entity);
+            entity = Map(await _repository.UpdateAsync(Map(entity)));
             return entity is null ? BadRequest() : Ok(entity);
+        }
+
+        private EventModel Map(EventRequest entity)
+        {
+            return _mapper.Map<EventModel>(entity);
+        }
+
+        private EventRequest Map(EventModel entity)
+        {
+            return _mapper.Map<EventRequest>(entity);
+        }
+
+        private IEnumerable<EventRequest> Map(IEnumerable<EventModel> entity)
+        {
+            return _mapper.Map<IEnumerable<EventRequest>>(entity);
+        }
+
+        private IEnumerable<EventModel> Map(IEnumerable<EventRequest> entity)
+        {
+            return _mapper.Map<IEnumerable<EventModel>>(entity);
         }
     }
 }
