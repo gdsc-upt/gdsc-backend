96e8a90fa2ab516537daefae87c30ee4f8dee75d
Andreea <andreea.bolea@student.upt.ro>
2021-06-14 21:13:32 +0300

    added Get by Id and authorization

--- GdscBackend/Controllers/v1/IdeasController.cs
+++ GdscBackend/Controllers/v1/IdeasController.cs
@@ -4,6 +4,7 @@ using System.Net.Mime;
 using System.Threading.Tasks;
 using GdscBackend.Database;
 using GdscBackend.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,13 +25,27 @@ namespace GdscBackend.Controllers.v1
         }
 
         [HttpGet]
+        [Authorize(Roles = "admin")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<IdeaModel>>> Get()
         {
             return Ok((await _repository.GetAsync()).ToList());
         }
+        
+        [HttpGet("{id}")]
+        [Authorize(Roles = "admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IdeaModel>> Get([FromRoute] string id)
+        {
+            var entity = await _repository.GetAsync(id);
+
+            return entity is null ? NotFound() : Ok(entity);
+        }
 
         [HttpPost]
+        [AllowAnonymous]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<IdeaModel>> Post(IdeaModel entity)
