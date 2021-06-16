f0047c51eb4eae1d11728456c694d5c660c0e863
Mihai <mihai1309f@gmail.com>
2021-06-16 22:31:19 +0300

    test: modified tests for ContactsController to include WebHookService

---
+++ GdscBackend.Tests/Mocks/TestWebhookService.cs
@@ -0,0 +1,22 @@
+﻿using GdscBackend.Utils.Services;
+using Xunit.Abstractions;
+
+namespace GdscBackend.Tests.Mocks
+{
+    public class TestWebhookService : IWebhookService
+    {
+        private readonly ITestOutputHelper _outputHelper;
+        public TestWebhookService(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        public void SendContact(string author, string mail, string subject, string message)
+        {
+            _outputHelper.WriteLine("Message sent by: " + author);
+            _outputHelper.WriteLine("With email: " + mail);
+            _outputHelper.WriteLine("Message subject: " + subject);
+            _outputHelper.WriteLine("Message content: " + message);
+        }
+    }
+}
